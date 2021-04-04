c1f2d60625e6a98b4dfb13fc7473aa4788a6d662
hngfu <djsl1018@naver.com>
2021-04-04 21:26:40 +0900

    🍎📱 Lottie animation 추가
    AuthViewController에 추가
    미어캣 애니메이션

--- Client/iOS/CheckPlusPlus/CheckPlusPlus/Authentication/AuthPickerViewController.swift
+++ Client/iOS/CheckPlusPlus/CheckPlusPlus/Authentication/AuthPickerViewController.swift
@@ -7,11 +7,21 @@
 
 import UIKit
 import FirebaseUI
+import Lottie
 
 class AuthPickerViewController: FUIAuthPickerViewController {
-
+    
+    @IBOutlet weak var backgroundAnimationView: AnimationView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.leftBarButtonItem = nil
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        backgroundAnimationView.loopMode = .loop
+        backgroundAnimationView.animationSpeed = 0.35    // slowly
+        backgroundAnimationView.play()
     }
 }
