932e4041cca0545cb5d948ed0f99c1bb8e976c10
hngfu <luke9844@gmail.com>
2021-04-11 11:15:22 +0900

    🍽 SessionManager 수정. Pooling 제거

--- Server/Session/SessionManager.cs
+++ Server/Session/SessionManager.cs
@@ -8,18 +8,13 @@ namespace Server
     {
         Dictionary<uint, ClientSession> _sessions = new Dictionary<uint, ClientSession>();
 
-        uint _sessionID = 5000;     // 0~4999는 pooling 예약
-        Queue<uint> _idPool = new Queue<uint>();
+        uint _sessionID = 0;
 
         public ClientSession Generate()
         {
             lock (_lock)
             {
-                uint id;
-                if (_idPool.Count > 0)
-                    id = _idPool.Dequeue();
-                else
-                    id = _sessionID++;
+                uint id = ++_sessionID;
 
                 ClientSession session = new ClientSession
                 {
@@ -36,28 +31,15 @@ namespace Server
         {
             lock (_lock)
             {
-                uint id = session.ID;
-                _sessions.Remove(id);
-                _idPool.Enqueue(id);
+                _sessions.Remove(session.ID);
             }
         }
 
-        #region singleton, lock, 초기화시 idPool 채움
+        #region singleton, lock
         static SessionManager _sessionManager = new SessionManager();
         public static SessionManager Instance { get { return _sessionManager; } }
 
         object _lock = new object();
-
-        private SessionManager()
-        {
-            lock (_lock)
-            {
-                for (uint i = 0; i < 5000; i++)
-                {
-                    _idPool.Enqueue(i);
-                }
-            }
-        }
         #endregion
     }
 }
